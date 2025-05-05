f21072356d1b73b1afa0d280e1eb7ac78818d258
Moulinie Luis <161343288+MoulinieLuis@users.noreply.github.com>
2025-05-05 21:46:36 +0000

    React Routes

--- tailwind-app/src/App.jsx
+++ tailwind-app/src/App.jsx
@@ -1,5 +1,9 @@
 import './App.css'
 import Card from './Card'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from './About';
+import ContactUs from './ContactUs';
+import TeamSection from './TeamSection'; 
 
 
 function App() {
@@ -55,16 +59,23 @@ function App() {
       properties: "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart",
     }];
   return (
-    <>
-    <div key="product-grid" className="bg-orange-500 p-8 grid grid-cols-4 gap-4">      
-        {products.map(e=><div key={e.model+"div"}><Card key={e.model} title={e.name} paragraph={e.properties} image={e.b64Image} model={e.model}/></div>)}
-    </div>
-    <div>
-    {/* <ContactUs/>
-    <TeamSection/>
-    <About/> */}
-    </div>
-    </>
+    
+    //...
+function App({props}){
+  //...
+  return(
+    //...
+    <BrowserRouter>
+      <Routes>
+        <Route path="Contact" element={<ContactUs/>} />
+        <Route path="Team" element={<TeamSection/>} />
+        <Route path="About" element={<About/>} />
+      </Routes>
+    </BrowserRouter>
+    //...
+  );
+  //..
+}
   )
 }
 
